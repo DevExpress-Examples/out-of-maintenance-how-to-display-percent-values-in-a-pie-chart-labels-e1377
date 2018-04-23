04d5ab606b25b2c289d1468fac464e08557f7b13
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to display percent values in a pie chart labels
+
+
+<p>This example demonstrates how to make series point labels of a <a href="http://devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument2967.htm"><u>pie chart</u></a> show their values as percentages.</p><p></p>
+
+
+<h3>Description</h3>
+
+<p>To achieve this at runtime, you should <i>cast</i> the series&#39;s <strong>PointOptions</strong> to the <strong>PiePointOptions</strong> type. A related tutorial can be found in our online documentation at <a href="http://help.devexpress.com/#XtraCharts/CustomDocument5858"><u>How to: Show Values as Percents in the Legend of a Pie Chart</u></a>.</p>
+
+<br/>
+
+
---
+++ VB/ValuesAsPercents/Form1.vb
@@ -0,0 +1,42 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace ValuesAsPercents
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' Create an empty chart.
+			Dim pieChart As New ChartControl()
+
+			' Create a pie series and add it to the chart.
+			Dim series1 As New Series("Pie Series", ViewType.Pie)
+			pieChart.Series.Add(series1)
+
+			' Add points to it.
+			series1.Points.Add(New SeriesPoint("A", New Double() { 0.3 }))
+			series1.Points.Add(New SeriesPoint("B", New Double() { 5 }))
+			series1.Points.Add(New SeriesPoint("C", New Double() { 9 }))
+			series1.Points.Add(New SeriesPoint("D", New Double() { 12 }))
+
+			' Make the series point labels display both arguments and values.
+			CType(series1.PointOptions, PiePointOptions).PointView = PointView.ArgumentAndValues
+
+			' Make the series points' values to be displayed as percents.
+			CType(series1.PointOptions, PiePointOptions).PercentOptions.ValueAsPercent = True
+			CType(series1.PointOptions, PiePointOptions).ValueNumericOptions.Format = NumericFormat.Percent
+			CType(series1.PointOptions, PiePointOptions).ValueNumericOptions.Precision = 0
+
+			' Add the chart to the form.
+			pieChart.Dock = DockStyle.Fill
+			Me.Controls.Add(pieChart)
+		End Sub
+
+	End Class
+End Namespace
